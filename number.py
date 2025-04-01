8e5473c98be6250344e33c0d70aae7d3c7e63d8e
tigerdeonis <tigerdeonis@gmail.com>
2025-04-01 08:53:56 +0200

    Update number.py

--- number.py
+++ number.py
@@ -1,6 +1,6 @@
 def check_number(n):
     """Определяет, является ли число четным или нечетным"""
-    if n % 2 == 0:
+    if n % 2 == 1:
         return f"Число {n} — четное."
     else:
         return f"Число {n} — число"
@@ -10,7 +10,7 @@ def main():
         number = int(input("Введите: "))
         print(check_number(number))
     except ValueError:
-        print("Ошибка: ")
+        print("Ошика: ")
 
 if __name__ == "__main__":
     main()
